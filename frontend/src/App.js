db46c8f125ff1a2a91437c9674129b83a380a3cd
Gavin <gavchan@users.noreply.github.com>
2020-02-11 12:38:10 +0800

    Add modal to handle CRUD functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Modal from "./components/Modal";
 
 // Generate several default todoItems
 const todoItems = [
@@ -26,18 +27,46 @@ const todoItems = [
     "created_at": "08/02/2020 10:01:01",
     "completed_at": null
   }
-]
+];
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      modal: false,
       viewCompleted: false,
+      activeItem: {
+        task: "",
+        description: "",
+        completed: false
+      },
       todoList: todoItems
     };
   }
 
-  displayCompleted = status=> {
+  toggle = () => {
+    this.setState({ modal: !this.state.modal });
+  };
+
+  handleSubmit = item => {
+    this.toggle();
+    alert("save" + JSON.stringify(item));
+  }
+
+  handleDelete = item => {
+    alert("delete" + JSON.stringify(item));
+  }
+
+  createItem = () => {
+    const item = { task: "", description: "", completed: false };
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  }
+
+  editItem = item => {
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  }
+
+  displayCompleted = status => {
     if (status) {
       return this.setState({ viewCompleted: true });
     }
@@ -66,7 +95,7 @@ class App extends Component {
   renderItems = () => {
     const { viewCompleted } = this.state;
     const newItems = this.state.todoList.filter(
-      item => item.completed == viewCompleted
+      item => item.completed === viewCompleted
     );
     return newItems.map(item => (
       <li
@@ -82,8 +111,12 @@ class App extends Component {
           {item.task}
         </span>
         <span>
-          <button className="btn btn-secondary mr-2"> Edit </button>
-          <button className="btn btn-danger"> Delete </button>
+          <button 
+            onClick={() => this.editItem(item)}
+            className="btn btn-secondary mr-2"> Edit </button>
+          <button 
+            onClick={() => this.handleDelete(item)}
+            className="btn btn-danger"> Delete </button>
         </span>
       </li>
     ));
@@ -96,7 +129,9 @@ class App extends Component {
           <div className="col-md-6 col-sm-10 mx-auto p-0">
             <div className="card p-3">
               <div className="">
-                <button className="btn btn-primary"> Add task </button>
+                <button 
+                  onClick={this.createItem}
+                  className="btn btn-primary"> Add task </button>
               </div>
               {this.renderTabList()}
               <ul className="list-group list-group-flush">
@@ -105,6 +140,13 @@ class App extends Component {
             </div>
           </div>
         </div>
+        {this.state.modal ? (
+          <Modal
+            activeItem={this.state.activeItem}
+            toggle={this.toggle}
+            onSave={this.handleSubmit}
+          />
+        ) : null}
       </main>
     )
   }
