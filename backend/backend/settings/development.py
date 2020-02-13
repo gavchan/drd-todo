e19ef6d24b5ab48298dfcd49e8801de090689d42
Gavin <gavchan@users.noreply.github.com>
2020-02-13 11:29:30 +0800

    Split Django settings to base and development

---
+++ backend/backend/settings/development.py
@@ -0,0 +1,10 @@
+from backend.settings.base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ')%%qph46_f#)fa5)1@+)2xp4nfj04i+aw4$v0ddi_kus!fp(e8'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["backend"]
+
